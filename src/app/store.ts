fef3860b4938306bb1a24a081f9adcb15ec3bb49
rus55 <r-mit@mail.ru>
2023-04-04 11:30:15 +0300

    todolist

---
+++ src/app/store.ts
@@ -0,0 +1,30 @@
+import {tasksReducer} from 'features/TodolistsList/tasks-reducer';
+import {todolistsReducer} from 'features/TodolistsList/todolists-reducer';
+import { AnyAction, applyMiddleware, combineReducers, createStore } from 'redux'
+import thunk, { ThunkAction, ThunkDispatch } from 'redux-thunk'
+import {appReducer} from './app-reducer'
+import {authReducer} from 'features/Login/auth-reducer'
+import {configureStore} from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer,
+    app: appReducer,
+    auth: authReducer
+})
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunk)
+})
+
+export type AppRootStateType = ReturnType<typeof rootReducer>
+
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AnyAction>
+
+export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, AnyAction>
+
+/*export const _store = createStore(rootReducer, applyMiddleware(thunkMiddleware));*/
+
+// @ts-ignore
+window.store = store;
